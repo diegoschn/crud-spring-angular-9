9afa7efa1eb7849ff3635bf2c950341f8bed90af
Diego Schneider <desenvolvedorjava87@gmail.com>
2020-12-02 04:39:56 -0300

    terminado login com spring security

--- clientes-api/src/main/java/br/com/crud/model/entity/Usuario.java
+++ clientes-api/src/main/java/br/com/crud/model/entity/Usuario.java
@@ -11,11 +11,11 @@ public class Usuario {
     private Integer id;
 
     @Column(unique = true, name = "login")
-    @NotEmpty(message = "${campo.login.obrigatorio}")
+    @NotEmpty(message = "{campo.login.obrigatorio}")
     private String username;
 
     @Column(name = "senha")
-    @NotEmpty(message = "${campo.senha.obrigatorio}")
+    @NotEmpty(message = "{campo.senha.obrigatorio}")
     private String password;
 
     public Integer getId() {
